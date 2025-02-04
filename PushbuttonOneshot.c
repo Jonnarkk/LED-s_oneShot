49a3612072998f7f74550d9105a502371de4bd62
Miller <guimigc@protonmail.com>
2025-02-04 19:43:19 -0300

    docs: implementando readme e comentando código

--- PushbuttonOneshot.c
+++ PushbuttonOneshot.c
@@ -2,17 +2,20 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
+// Defines
 #define red_pin 13
 #define blue_pin 12
 #define green_pin 11
 #define botao_A 5
 
 //Variáveis globais
-volatile uint8_t leds_active = 0;
-uint32_t last_time = 0;
-volatile bool botao_press = false;
-bool timer = false;
+volatile uint8_t leds_active = 0; // Contador para situar qual LED deve apagar
+uint32_t last_time = 0; // Variável para realização do debouncing
+volatile bool botao_press = false; // Booleano para verificar aperto do botão
+bool timer = false; // Booleano para verificar se timer está ativo
 
+
+// Função para inicializar todos os pinos necessários
 void inicializar_pinos(){
     gpio_init(red_pin);
     gpio_set_dir(red_pin, GPIO_OUT);
@@ -25,6 +28,7 @@ void inicializar_pinos(){
     gpio_pull_up(botao_A);
 }
 
+// Função onde o temporizador oneshot realiza sua lógica
 int64_t turn_off_callback(alarm_id_t id, void *user_data){
     switch (leds_active){
     case 0:
@@ -53,6 +57,7 @@ int64_t turn_off_callback(alarm_id_t id, void *user_data){
     
 }
 
+// Função da interrupção onde está situada a lógica para aperto do botão
 void button_callback(uint gpio, uint32_t events) {
     static uint32_t last_time = 0;
     uint32_t current_time = to_ms_since_boot(get_absolute_time());
@@ -71,7 +76,7 @@ int main(){
     stdio_init_all();
     inicializar_pinos();
 
-    gpio_set_irq_enabled_with_callback(botao_A, GPIO_IRQ_EDGE_FALL, true, &button_callback);
+    gpio_set_irq_enabled_with_callback(botao_A, GPIO_IRQ_EDGE_FALL, true, &button_callback); // Função que configura a interrupção do botão
 
     while (true) {
         if (botao_press && leds_active == 0) {
@@ -84,10 +89,10 @@ int main(){
             gpio_put(blue_pin, 1);
 
         // Inicia o alarme de 3 segundos
-        add_alarm_in_ms(3000, turn_off_callback, NULL, true);
+        add_alarm_in_ms(3000, turn_off_callback, NULL, true); // Função de configuração do temporizador oneshot
         }
 
-        sleep_ms(10);
+        sleep_ms(10); // Pausa para diminuir a frequência de operação
 
     }
 
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# LED-s_oneShot
-Utilização da temporização oneshot para ativação de LED's
+# LED's_oneShot
+Este projeto consiste na utilização de um temporizador one-shot (disparo único) para a simulação de um sistema de LED's com um botão.
+
+## Descrição de funcionamento
+Após pressionar o botão, todos os LED's são ativados, após 3 segundos, um dos LED's é desativado, após outros 3 segundos, um outro LED é também desativado, o processo se repete até que 
+todos os leds estejam apagados. Durante o processo de desligamento dos LED's, o botão não terá efeito e não poderá ativar todos os LED's novamente, ou seja, será **ignorado** o sinal do
+botão até que todos os LED's apaguem.
+
+## Estrutura do código
+O código está organizado da seguinte maneira:
+
+- `inicializar_pinos()`: Inicializa os pinos necessários.
+- `int64_t turn_off_callback(alarm_id_t id, void *user_data)`: Realiza lógica após temporizador one-shot ser configurado.
+- `button_callback(uint gpio, uint32_t events)`: Realiza lógica da interrupção quando o botão é pressionado.
+- `gpio_set_irq_enabled_with_callback(botao_A, GPIO_IRQ_EDGE_FALL, true, &button_callback)`: Configura interrupção do botão para borda de descida.
+- `add_alarm_in_ms(3000, turn_off_callback, NULL, true)`: Configura temporizador one-shot para chamar função `int64_t turn_off_callback(alarm_id_t id, void *user_data)` em 3 segundos.
+
+## Desenvolvedor
+Guilherme Miller Gama Cardoso
